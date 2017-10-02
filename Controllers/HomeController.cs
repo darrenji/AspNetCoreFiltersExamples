147fff0778b6fcd58322d09d387283cacf0cfbda
darren <764190362@qq.com>
2017-10-02 21:16:01 +0800

    在Action中实现过滤

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AspNetCoreFiltersExamples.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: /<controller>/
+        //public IActionResult Index()
+        //{
+        //    return View("Message", "this is the Index action from the Home Controller");
+        //}
+
+
+        public IActionResult Index()
+        {
+            if(!Request.IsHttps)
+            {
+                return new StatusCodeResult(StatusCodes.Status403Forbidden);
+            }
+            else
+            {
+                return View("Message", "This is the Index action on the Home Controller");
+            }
+        }
+    }
+}
